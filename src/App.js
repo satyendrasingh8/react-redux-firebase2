85b84c47d03658ad0147d85c24bcf805303828ee
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-08-28 14:04:45 -0700

    conmmited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './projects/CreateProject';
 
 function App() {
   return (
+    <BrowserRouter >
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+   <Switch>
+  <Route exact path="/" component={Dashboard} />
+  <Route path="/project/:id" component={ProjectDetails} />
+  <Route  path="/signin" component={SignIn} />
+  <Route path="/signup" component={SignUp} />
+  <Route path="/create" component={CreateProject} />
+   </Switch>
+
     </div>
+
+
+
+
+
+    
+    </BrowserRouter>
   );
 }
 
